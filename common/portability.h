4996a18ae44a219adbe525e6a967447190aa5eb2
dkaznadzey@gmail.com <dkaznadzey@gmail.com@3c69001b-b14c-0410-a418-21cc06c7a29f>
2011-05-27 01:28:17 +0000

    Reapplied former fixes: - CIGAR string - 1-based similarity positions - space reuse for subject sequences - reverse- and forward- only search for NA and NN
    Fixed psimscan negative alignment zone start bug
    
    TODO: save subj. length in result record

--- common/portability.h
+++ common/portability.h
@@ -25,39 +25,49 @@
 // long file support flavours for different platforms
 #if defined (_MSC_VER)
     #include <io.h>
-	#define sci_stat _stati64
-	#define sci_stat_struc _stati64
-	#define sci_open _open
-	#define sci_read _read
-	#define sci_write _write
-	#define sci_close _close
-	#define sci_lseek _lseeki64
-	#define sci_tell _telli64
+    #define sci_stat _stati64
+    #define sci_stat_struc _stati64
+    #define sci_open _open
+    #define sci_read _read
+    #define sci_write _write
+    #define sci_close _close
+    #define sci_lseek _lseeki64
+    #define sci_tell _telli64
 #elif defined (__CYGWIN__)
     #include <unistd.h>
-	#define sci_stat stat
-	#define sci_stat_struc stat
-	#define sci_open open
-	#define sci_read read
-	#define sci_write write
-	#define sci_close close
-	#define sci_lseek lseek
-	#define sci_tell tell
+    #define sci_stat stat
+    #define sci_stat_struc stat
+    #define sci_open open
+    #define sci_read read
+    #define sci_write write
+    #define sci_close close
+    #define sci_lseek lseek
+    #define sci_tell tell
+#elif defined (__MACOSX__)
+    #include <unistd.h>
+    #define sci_stat stat
+    #define sci_stat_struc stat
+    #define sci_open open
+    #define sci_read read
+    #define sci_write write
+    #define sci_close close
+    #define sci_lseek lseek
+    #define sci_tell tell
 #else // plain unix :)
     #include <unistd.h>
-	#define sci_stat stat64
-	#define sci_stat_struc stat64
-	#define sci_open open64
-	#define sci_read read
-	#define sci_write write
-	#define sci_close close
-	#define sci_lseek lseek64
-	#define sci_tell tell64
+    #define sci_stat stat64
+    #define sci_stat_struc stat64
+    #define sci_open open64
+    #define sci_read read
+    #define sci_write write
+    #define sci_close close
+    #define sci_lseek lseek64
+    #define sci_tell tell64
 #endif
 
 #if defined (_MSC_VER)
-	#define strncasecmp strnicmp
-	#define strcasecmp stricmp
+    #define strncasecmp strnicmp
+    #define strcasecmp stricmp
 #endif
 
 #ifndef O_BINARY
