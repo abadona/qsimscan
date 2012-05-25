2dc02f3504860ff18b4b4e08d7b607ea79f867ab
dkaznadzey@gmail.com <dkaznadzey@gmail.com@3c69001b-b14c-0410-a418-21cc06c7a29f>
2012-05-25 21:33:28 +0000

    adding files needed for just submitted cumulative changes

---
+++ common/tracer.cpp
@@ -0,0 +1,69 @@
+//////////////////////////////////////////////////////////////////////////////
+// This software module is developed by SCIDM team in 1999-2011.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// For any questions please contact SciDM team by email at scidmteam@yahoo.com
+//
+// This module is modified for use in PDOP MexSat project by Hughes Network Systems, LLC
+//
+//////////////////////////////////////////////////////////////////////////////
+#include "tracer.h"
+#include <ctime>
+
+#include <iostream>
+
+static const int TMBUF_SZ = 64;
+std::ostream& operator << (std::ostream& e, Trace__ t)
+{
+    struct tm* timeinfo;
+    char tmbuffer [TMBUF_SZ];
+
+    timeinfo = localtime (&(t.time_));
+    strftime (tmbuffer, TMBUF_SZ, "[%x %X %Z]", timeinfo);
+
+    if (t.func_)  e << t.func_ << ": ";
+    if (t.fname_) e << t.fname_ << ":" << t.lno_ << ": ";
+    if (t.func_) e << "thr " << t.thread_ << ":";
+    e << tmbuffer << " ";
+    return e;
+}
+
+Logger::Logger (bool enabled)
+:
+logger_on_ (enabled),
+o_ (std::cerr)
+{
+}
+
+Logger::Logger (std::ostream& o, bool enabled)
+:
+logger_on_ (enabled),
+o_ (o)
+{
+}
+
+
+Logger trclog (false);
+Logger dbglog (false);
+Logger info (false);
+Logger warnlog (true);
+Logger errlog (true);
+
+void set_logging_level (Logger::LEVEL level)
+{
+    trclog.enable   (level >= Logger::TRACE);
+    dbglog.enable   (level >= Logger::DEBUG);
+    info.enable  (level >= Logger::INFO);
+    warnlog.enable  (level >= Logger::WARNING);
+    errlog.enable   (level >= Logger::ERROR);
+}
+
---
+++ common/tracer.h
@@ -0,0 +1,119 @@
+//////////////////////////////////////////////////////////////////////////////
+// This software module is developed by SCIDM team in 1999-2011.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// For any questions please contact SciDM team by email at team@scidm.org
+//
+// This module is modified for use in PDOP MexSat project by Hughes Network Systems, LLC
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef __tracer_h__
+#define __tracer_h__
+
+#include <ostream>
+#include <iomanip>
+#include <ctime>
+#include <pthread.h>
+#include "common_str.h"
+
+
+class Trace__
+{
+public:
+    const char* fname_;
+    int lno_;
+    const char* func_;
+    time_t time_;
+    pthread_t thread_;
+
+    Trace__ (const char* fname = NULL, int lno = 0, const char* funcname = NULL)
+    :
+    fname_ (fname),
+    lno_ (lno),
+    func_ (funcname),
+    time_ (time (NULL)),
+    thread_ (pthread_self ())
+    {
+    }
+};
+
+
+std::ostream& operator << (std::ostream& e, Trace__ t);
+
+#define Trace Trace__(__FILE__, __LINE__, __FUNCTION__)
+
+class Logger
+{
+    bool logger_on_;
+public:
+
+    enum LEVEL
+    {
+        CRITICAL = 0,
+        ERROR   = 10,
+        WARNING = 20,
+        INFO    = 30,
+        DEBUG   = 40,
+        TRACE   = 50,
+    };
+
+    std::ostream& o_;
+
+    Logger (bool enabled = false);
+    Logger (std::ostream& o, bool enabled = true);
+
+    void enable  (bool op = true) { logger_on_ = op;  }
+    void disable () { logger_on_ = false; }
+    bool enabled () const { return logger_on_; }
+};
+
+const Logger::LEVEL loglevels [] = {Logger::CRITICAL, Logger::ERROR, Logger::WARNING, Logger::INFO, Logger::DEBUG, Logger::TRACE};
+
+
+// output operator support
+template <class TT>
+Logger& operator << (Logger& logger, const TT& operand)
+{
+    if (logger.enabled ())
+        logger.o_ << operand;
+    return logger;
+}
+// ostream manipulators support
+inline Logger& operator << (Logger& logger, std::ostream& (*op) (std::ostream&))
+{
+    if (logger.enabled ())
+        logger.o_ << op;
+    return logger;
+}
+
+#define clreol "\x1B[K"
+
+#ifndef __tracer_cpp__
+extern Logger trclog;   // common tracing, off by default
+extern Logger dbglog;
+extern Logger info;
+extern Logger warnlog;
+extern Logger errlog;
+#endif
+
+#define trc (trclog << Trace)
+#define dbg (dbglog << Trace << "Debug: ")
+#define errl (errlog << Trace << "Error: ")
+#define warn (warnlog << "Warning: ")
+
+// convinience function setting logging level
+void set_logging_level (Logger::LEVEL level);
+
+#define IND(x) std::setw (x) << EMPTY_STR << std::setw (0)
+
+
+#endif
